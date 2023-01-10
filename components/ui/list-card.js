e201b36da51269d62ce00468332a8306edde6793
rock-zion <rock_zion@outlook.com>
2023-01-10 21:08:13 +0100

    add static elements from list card

--- components/ui/list-card.js
+++ components/ui/list-card.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import styles from './list-card.module.scss';
+import Add from 'assets/images/add.svg';
+import classNames from 'classnames';
 
 const ListCard = props => {
-  const { Icon, listItems } = props;
+  const { Icon, listItems, title } = props;
   return (
     <div className={styles.wrapper}>
-      <div></div>
+      <div className={styles.header}>
+        <span className={'text_body'}>{title}</span>
+        <button className={''}>
+          <Add />
+        </button>
+      </div>
+
+      {!listItems.length && (
+        <div className={styles.empty_state}>
+          <Icon />
+        </div>
+      )}
     </div>
   );
 };
