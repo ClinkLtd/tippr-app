2197ce218de2486b5dcfc6dcbc4cb1c2ecbb8c05
rock-zion <rock_zion@outlook.com>
2023-01-10 19:56:13 +0100

    create list-card component

---
+++ components/ui/list-card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './list-card.module.scss';
+
+const ListCard = props => {
+  const { Icon, listItems } = props;
+  return (
+    <div className={styles.wrapper}>
+      <div></div>
+    </div>
+  );
+};
+
+export default ListCard;
