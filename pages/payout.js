2705c3c8016ad3ba173ec62c469ebefb39c55be0
rock-zion <rock_zion@outlook.com>
2023-01-11 17:07:20 +0100

    create tippr payout page

---
+++ pages/payout.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import Buttons from 'components/forms/buttons';
+import Transactions from 'components/ui/transactions';
+
+import { useRouter } from 'next/router';
+
+import Share from 'assets/images/share-white.svg';
+import Twitter from 'assets/images/twitter-dark.svg';
+import Whatsapp from 'assets/images/whatsapp-dark.svg';
+import Coins from 'assets/images/coins-stacked.svg';
+import Edit from 'assets/images/edit-dark.svg';
+
+export default function LinkBankAccount() {
+  const router = useRouter();
+
+  return (
+    <div className={'main-container'}>
+      <div className='flex justify-between items-center mb-5'>
+        <div className='flex flex-col'>
+          <span className='text_body'>Your unpaid balance</span>
+          <span className='text_xl'>₦45,192.18</span>
+        </div>
+        <div className='flex flex-col items-end'>
+          <span className='text_body'>Share Tippr</span>
+          <div className='flex'>
+            <Twitter />
+            <Whatsapp />
+          </div>
+        </div>
+      </div>
+
+      <div className='mb-20'>
+        <Buttons
+          style='mb-2.5'
+          square={true}
+          state='auth'
+          text='Payout history'
+          Icon={Coins}
+        />
+        <Buttons
+          square={true}
+          state='auth'
+          text='Manage your Tippr page'
+          Icon={Edit}
+        />
+      </div>
+
+      <Buttons state='primary' text='Share your Tippr Link' Icon={Share} />
+
+      <div className='mt-5'>
+        <span className='text_body mb-2.5'>Tip history</span>
+        <Transactions />
+        <Transactions />
+        <Transactions />
+        <Transactions />
+        <Transactions />
+        <Transactions />
+      </div>
+    </div>
+  );
+}
