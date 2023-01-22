a12d14baf953896dd2ccaf77ccde28f5ccf365a3
rock-zion <rock_zion@outlook.com>
2023-01-23 00:01:33 +0100

    Update link-bank-account.js

--- pages/auth/link-bank-account.js
+++ pages/auth/link-bank-account.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from 'styles/pages/auth.module.scss';
 import Buttons from 'components/forms/buttons';
 import { useRouter } from 'next/router';
@@ -6,32 +6,35 @@ import { useRouter } from 'next/router';
 import Bank from 'assets/images/bank.svg';
 import Arrow from 'assets/images/arrow-right.svg';
 import TipprInput from 'components/forms/tippr-input';
+import TipprSelect from 'components/forms/tippr-select';
 
 export default function LinkBankAccount() {
   const router = useRouter();
 
   return (
-    <div className={styles.auth}>
-      <Bank />
-      <h1 className='text_header mt-2.5'>Link a Bank Account</h1>
-      <p className='text_body mt-1'>
-        You need to link a bank account to verify your identity and enable you
-        receive payments on Tippr
-      </p>
+    <Fragment>
+      <div className={styles.auth}>
+        <Bank />
+        <h1 className='text_header mt-2.5'>Link a Bank Account</h1>
+        <p className='text_body mt-1'>
+          You need to link a bank account to verify your identity and enable you
+          receive payments on Tippr
+        </p>
 
-      <div>
-        <TipprInput />
-        <TipprInput />
-      </div>
+        <div className='mt-20'>
+          <TipprInput label='Account number' placeholder='0142468037' />
+          <TipprSelect label='Bank' />
+        </div>
 
-      <div className={styles.footer}>
-        <Buttons
-          handleClick={() => router.push('/auth/tippr')}
-          state='primary'
-          text='Continue'
-          Icon={Arrow}
-        />
+        <div className={styles.footer}>
+          <Buttons
+            handleClick={() => router.push('/auth/tippr')}
+            state='primary'
+            text='Continue'
+            Icon={Arrow}
+          />
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
